8ce3b636dd0cf611bb2808f28a1b6c3bd94aeb19
kuroyakov <k.kuroya@gmail.com>
2017-02-01 23:59:46 +0900

    一通りの例を試した
    * 関数
    * 部分適用
    * 合成
    * 集合と関数適用

--- FableStudy/test.fsx
+++ FableStudy/test.fsx
@@ -1,1 +1,21 @@
+// まずは直接の命令文
 printfn "hello, fable"
+
+// 次は変数束縛とその値の出力
+let x = 10
+printfn "x=%A" x
+
+// 関数
+let f x y = x + y
+f 10 20 |> printfn "x+y=%A" 
+
+// 部分適用
+let add10 = f 10
+add10 50 |> printfn "50 add 10 = %A"
+
+// 合成
+let negiate x = -x
+20 |> (add10>>negiate>>string) |> printfn "20 add 10 and negiate=%A"
+
+// 配列
+[1..50] |> List.map add10 |> printfn "%A"
--- FableStudy/test.js
+++ FableStudy/test.js
@@ -1,7 +1,62 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.add10 = exports.x = undefined;
+exports.f = f;
+exports.negiate = negiate;
+
 var _String = require("fable-core/umd/String");
+
+var _List = require("fable-core/umd/List");
+
+var _Seq = require("fable-core/umd/Seq");
 
 (0, _String.fsFormat)("hello, fable")(function (x) {
   console.log(x);
 });
+var x = exports.x = 10;
+(0, _String.fsFormat)("x=%A")(function (x) {
+  console.log(x);
+})(x);
+
+function f(x_1, y) {
+  return x_1 + y;
+}
+
+(0, _String.fsFormat)("x+y=%A")(function (x) {
+  console.log(x);
+})(f(10, 20));
+
+var add10 = exports.add10 = function () {
+  var x_1 = 10;
+  return function (y) {
+    return f(x_1, y);
+  };
+}();
+
+(0, _String.fsFormat)("50 add 10 = %A")(function (x) {
+  console.log(x);
+})(add10(50));
+
+function negiate(x_1) {
+  return -x_1;
+}
+
+(0, _String.fsFormat)("20 add 10 and negiate=%A")(function (x) {
+  console.log(x);
+})(function ($var2) {
+  return function (value) {
+    return String(value);
+  }(function ($var1) {
+    return function (x_1) {
+      return negiate(x_1);
+    }(add10($var1));
+  }($var2));
+}(20));
+(0, _String.fsFormat)("%A")(function (x) {
+  console.log(x);
+})(function (list) {
+  return (0, _List.map)(add10, list);
+}((0, _Seq.toList)((0, _Seq.range)(1, 50))));
